49163afb912da6dffe10fa7b36b2c987a2bbe4e0
Lemnver <emanuel.swenson@protonmail.ch>
2020-10-01 13:27:31 +0000

    nya

--- server/enviroment/ctrace/ctrace/settings.py
+++ server/enviroment/ctrace/ctrace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '^n63q(*7mx9(p0dyz@3pn6&+7zxdv!&jfmq#&5y5671knxx8_i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app.zenofob.com',]
 
 AUTH_USER_MODEL = 'core.user'
 
@@ -132,3 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
