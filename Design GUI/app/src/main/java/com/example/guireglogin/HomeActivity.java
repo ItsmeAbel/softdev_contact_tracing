445a65e937e685bd522f7f18bb3399299acf4562
Erik Armensjo <erik.armensjo@gmail.com>
2020-10-21 16:59:24 +0200

    La till datum tracking

--- Design GUI/app/src/main/java/com/example/guireglogin/HomeActivity.java
+++ Design GUI/app/src/main/java/com/example/guireglogin/HomeActivity.java
@@ -14,6 +14,8 @@ import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -37,6 +39,10 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
@@ -55,6 +61,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     public int toggler = 0;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
